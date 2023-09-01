05bcdc04fb2e827d2be32266e77b51c9a5ef2135
Rooney Lee <kroonstazy@gmail.com>
2023-09-01 16:46:33 +0900

    Assignment 3 게시글 등록하기

--- donghoon/westagram_backend.js
+++ donghoon/westagram_backend.js
@@ -57,11 +57,12 @@ const addUser = async (req, res) => {
         //3. DATABASE로 정보 저장
         const userData = await myDataSource.query(
             `INSERT INTO users (name, email, password) VALUES ("${name}", "${email}", "${password}");`)
-        return res.status(201).json({ "message" : "userSuccessfully Added"});
+        return res.status(201).json({ "message" : "userCreated"});
     } catch (err) {
         console.log(err);
     }
 };
+// {"name":"동훈", "password":"자동차좋아요", "email": "email@email.com"}
 
 const delUser = async (req, res) => {
     try {
@@ -83,17 +84,26 @@ const updateUser = async (req, res) => {
 };
 
 const addPost = async (req, res) => {
-    const postSuccessMsg = [
-        { message: "postCreated" }
-    ];
-    const postData = req.body;
-    posts[0].data.push(postData)
     try {
-        return res.status(201).json(postSuccessMsg);
+        //1. 유저 정보를 FE에서 받는다
+    const newPostData = req.body
+    console.log(newPostData);
+    const title = newPostData.title
+    console.log(title)
+    const content = newPostData.content
+    console.log(content)
+    const user_id = newPostData.user_id
+    console.log(user_id)
+    // const age = newUserData.age
+    // console.log(age)
+    //3. DATABASE로 정보 저장
+    const userData = await myDataSource.query(
+        `INSERT INTO posts (title, content, user_id) VALUES ("${title}", "${content}", "${user_id}");`)
+    return res.status(201).json({ "message" : "postCreated"});
     } catch (err) {
         console.log(err);
     }
-};
+}; // {"title":"'mycli 개꿀이다'", "content":"동언님 왈, 달다 달아 너무 달아서 이가 썩을것 같다.", "user_id": 6}
 
 const showPosts = async (req, res) => {
     try {
@@ -111,14 +121,12 @@ app.get("/", welcome);
 //1. API 로 users 화면에 보여주기
 app.get("/users", getUser);
 
-//2. users 생성 (게시글 등록하기)
+// Assignment 2. 유저 회원가입 하기
 app.post("/add/users", addUser)
-// {"id": 3, "name": "이동훈", "email": "kroonstazy@gmail.com", "password": "password"}
-//2-1. 게시글 등록하기
-app.post("/add/posts", addPost);
-// {"userID" : 5, "userName" : "동훈", "postingId" : 5, "postingTitle" : "어서와 새로운 타이틀이야", "postingContent" : "This is the content of the new post 5."}
 
-// 과제 3 DELETE
+// Assignment 3. 게시글 등록하기 
+app.post("/add/posts", addPost);
+
 // 가장 마지막 user를 삭제하는 엔드포인트
 app.delete("/del/users", delUser);
 
