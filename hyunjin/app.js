56d810eef626b6c3af7ce1a829e37efb6482045c
hyunjin <alfma123@naver.com>
2023-09-03 20:51:35 +0900

    Style: 파라미터 컨벤션 통일

--- hyunjin/app.js
+++ hyunjin/app.js
@@ -78,14 +78,14 @@ class App {
     });
   }
   status404() {
-    this.app.use((req, res, next) => {
+    this.app.use((req, _, next) => {
       const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
       error.status = 404;
       next(error);
     });
   }
   errorHandler() {
-    this.app.use((err, req, res, next) => {
+    this.app.use((err, _, res, next) => {
       res.locals.message = err.message;
       res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
       res.status(err.status || 500);
