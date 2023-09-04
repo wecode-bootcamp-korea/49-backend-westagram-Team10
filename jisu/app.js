2cb6ef5dd53d32231bfe8e4eb9a3f07df42c88d3
JISUlicious <ljshong@hanmail.net>
2023-09-04 12:22:23 +0900

    ADD: getUserPosts, update/deletePost,toggleLike

--- jisu/app.js
+++ jisu/app.js
@@ -47,14 +47,23 @@ const getUsers = async (req, res) => {
 
 const createUser = async (req, res) => {
   try {
-    const newUser =req.body;
+    const newUser = req.body;
+    const duplicateEmail = await appDataSource.query(
+      `SELECT email FROM users WHERE email = '${newUser.email}';`
+    );
+    
+    if (duplicateEmail.length > 0) {
+      return res.status(400).json({ "error": "Duplicate user email"});
+    }
+
     const result = await appDataSource.query(
       `INSERT INTO users
       (name, email, password, profile_image)
       VALUES
       ('${newUser.name}', '${newUser.email}',
       '${newUser.password}', '${newUser.profile_image}')`);
-    return res.status(201).json({ "message": "userCreated" });
+    
+      return res.status(201).json({ "message": "userCreated" });
   } catch (error) {
     console.log(error);
   }
@@ -65,9 +74,9 @@ const createPost = async (req, res) => {
     const body = req.body;
     const result = await appDataSource.query(
       `INSERT INTO posts
-      (title, content, user_id, post_image_url)
+      (content, user_id, post_image_url)
       VALUES
-      ('${body.title}', '${body.content}',
+      ('${body.content}',
       '${body.user_id}', '${body.post_image_url}')`);
     return res.status(201).json({ "message": "postCreated" });
   } catch (error) {
@@ -93,11 +102,126 @@ const getPosts = async (req, res) => {
   }
 };
 
+const getUserPosts = async (req, res) => {
+  try {
+    const user = await appDataSource.query(
+      `SELECT
+      users.id AS userId,
+      users.profile_image AS userProfileImage
+      FROM users
+      WHERE users.id = ${req.params.id};`
+    );
+    const posts = await appDataSource.query(
+      `SELECT
+      posts.id AS postingId,
+      posts.post_image_url AS postingImageURL,
+      posts.content AS postingContent
+      FROM posts
+      WHERE posts.user_id = ${req.params.id};`
+    );
+    result = user[0];
+    result['postings'] = posts;
+    return res.status(200).json({ "data": result });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updatePost = async (req, res) => {
+  try {
+    const body = req.body;
+    const oldPost = await appDataSource.query(
+      `SELECT * 
+      FROM posts 
+      WHERE posts.id = ${req.params.id};`
+    );
+
+    if (oldPost[0].user_id !== body.user_id) {
+      return res.status(401).json({ "error": "Unauthorized" });
+    }
+    
+    const updated = await appDataSource.query(
+      `UPDATE posts
+      SET content = '${body.content}',
+      post_image_url = '${body.post_image_url}'
+      WHERE posts.id = ${req.params.id};`);
+
+    const result = await appDataSource.query(
+      `SELECT
+      users.id AS userId,
+      users.name AS userName,
+      posts.id AS postingId,
+      posts.post_image_url AS postingImageURL,
+      posts.content AS postingContent
+      FROM users
+      JOIN posts ON posts.user_id = users.id
+      WHERE posts.id = ${req.params.id};`
+    );
+    
+    return res.status(200).json({ "data": result });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const deleteFromLikes = await appDataSource.query(
+      `DELETE FROM likes
+      WHERE post_id = ${postId}`
+    );
+    
+    const result = await appDataSource.query(
+      `DELETE FROM posts
+      WHERE posts.id = ${postId};`
+    );
+    
+    return res.status(200).json({ "message": "postingDeleted" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const toggleLike = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.body.user_id;
+    const duplicateLike = await appDataSource.query(
+      `SELECT * FROM likes
+      WHERE user_id = ${userId} AND post_id = ${postId};`
+    );
+
+    if (duplicateLike.length > 0) {
+      const removed = await appDataSource.query(
+        `DELETE FROM likes
+        WHERE likes.id = ${duplicateLike[0].id};`
+      );
+      return res.status(200).json({ "message": "likeDeleted" });
+    }
+
+    const result = await appDataSource.query(
+      `INSERT INTO likes
+      (user_id, post_id)
+      VALUES
+      ('${userId}', '${postId}');`
+    );
+    
+    return res.status(200).json({ "message": "likeCreated" });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 app.get('/', getGreeting);
 app.get('/users', getUsers);
 app.post('/users', createUser);
 app.post('/posts', createPost);
 app.get('/posts', getPosts);
+app.get('/users/posts/:id', getUserPosts);
+app.put('/posts/:id', updatePost);
+app.delete('/posts/:id', deletePost);
+app.post('/likes/:id', toggleLike);
 
 const server = http.createServer(app);
 
