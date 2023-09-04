0df2030ddfd0da1ae841e1a58b458d496fd8cd9d
Rooney Lee <kroonstazy@gmail.com>
2023-09-04 17:32:39 +0900

    Assignment 7 완료

--- donghoon/westagram_backend.js
+++ donghoon/westagram_backend.js
@@ -1,26 +1,31 @@
 const http = require("http");
 const express = require("express");
-const { DataSource } = require("typeorm")
+const morgan = require('morgan');
+const cors = require('cors');
+const { DataSource } = require("typeorm");
+const dotenv = require('dotenv').config();
 
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
+const app = express();
+const express_port = 8000;
+
 //DB Connection via TypeORM
 const myDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "root",
-    password: "",
-    database: "westargram"
+    type: process.env.TYPEORM_CONNECTION,
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE
 });
 
-const app = express();
+app.use(cors());//모든 request에 대해 CORS 요청을 설정하는 법
+app.use(morgan('combined')); 
 
 app.use(express.json()); // for parsing application/json
 app.use(bodyParser.json());
-
-
 
 // 기능함수 생성
 const welcome = async (req, res) => {
@@ -45,13 +50,13 @@ const addUser = async (req, res) => {
     try {
         //1. 유저 정보를 FE에서 받는다
         const newUserData = req.body
-        console.log(newUserData);
+        // console.log(newUserData);
         const name = newUserData.name
-        console.log(name)
+        // console.log(name)
         const password = newUserData.password
-        console.log(password)
+        // console.log(password)
         const email = newUserData.email
-        console.log(email)
+        // console.log(email)
         // const age = newUserData.age
         // console.log(age)
         //3. DATABASE로 정보 저장
@@ -85,15 +90,15 @@ const updateUser = async (req, res) => {
 
 const addPost = async (req, res) => {
     try {
-        //1. 유저 정보를 FE에서 받는다
+//1. 유저 정보를 FE에서 받는다
     const newPostData = req.body
-    console.log(newPostData);
+    // console.log(newPostData);
     const title = newPostData.title
-    console.log(title)
+    // console.log(title)
     const content = newPostData.content
-    console.log(content)
+    // console.log(content)
     const user_id = newPostData.user_id
-    console.log(user_id)
+    // console.log(user_id)
     // const age = newUserData.age
     // console.log(age)
     //3. DATABASE로 정보 저장
@@ -107,12 +112,50 @@ const addPost = async (req, res) => {
 
 const showPosts = async (req, res) => {
     try {
-        return res.status(200).json(posts);
+        const postsData = await myDataSource.query("SELECT posts.user_id, users.profile_image AS userProfileImage, posts.id AS postingId, posts.content AS postingContent FROM posts JOIN users ON posts.user_id = users.id;");
+        console.log(postsData);
+        return res.status(200).json({ "Data" : postsData })
     } catch (err) {
         console.log(err);
     }
 };
 
+
+const searchPostsByUserId = async (req, res) => {
+    try {
+        const userId = req.params.user_id;
+        // console.log("유저아이디가 뭡니까?" + userId);
+        const userPosts = await myDataSource.query (`SELECT * FROM posts WHERE posts.user_id = ${userId}`)
+        return res.status(200).json({"Data" : {userPosts}})
+    } catch (err) {
+        console.log(err)
+    }
+};
+
+const updatePost = async (req, res) => {
+    try {
+        const postId = req.params.post_id;
+        const content_string = req.body.content;
+        console.log(postId);
+        console.log()
+        console.log(content_string)
+        const updatePost = await myDataSource.query(`UPDATE posts SET content = ${content_string} WHERE posts.id = ${postId}`)
+        console.log(postId);
+        return res.status(200).json({ updatePost })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+const delPosts = async (req, res) => {
+    try {
+        const postId = req.params.post_id;
+        const deletePost = await myDataSource.query(`DELETE FROM posts WHERE id = ${postId}`)
+        return res.status(204).json({"message" : "postingDeleted"})
+    } catch (err) {
+        console.log(err)
+    }
+}
 // API Lists
 
 //0. Server Launch Message
@@ -126,6 +169,20 @@ app.post("/add/users", addUser)
 
 // Assignment 3. 게시글 등록하기 
 app.post("/add/posts", addPost);
+
+// Assignment 4. 전체 게시글 조회하기
+app.get('/posts', showPosts)
+
+// Assignment 5. 유저의 게시글 조회하기
+app.get('/posts/:user_id', searchPostsByUserId) // /posts/"숫자" 로 get요청!
+
+// Assignment 6. 게시글 수정하기
+app.post('/update/posts/:post_id', updatePost)
+
+// Assignment 7. 게시글 삭제하기
+app.put('/del/posts/:post_id', delPosts)
+
+// Assignment 8. 좋아요 누르기
 
 // 가장 마지막 user를 삭제하는 엔드포인트
 app.delete("/del/users", delUser);
@@ -149,7 +206,7 @@ const server = http.createServer(app); // express app 으로 서버를 만듭니
 const start = async () => {
     // 서버를 시작하는 함수입니다.
     try {
-        server.listen(8000, () => console.log(`Server is listening on 8000`));
+        server.listen(express_port, () => console.log(`Server is listening on ${express_port}`));
     } catch (err) {
         console.error(err);
     }
