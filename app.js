f30d20e0423940b732b12f20ba947b3440dd2ebb
hyeonwoo Jeong <heywoo12@gmail.com>
2023-09-04 18:08:00 +0900

    ADD : 유저 회원가입 하기

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 const http = require('http')
 const express = require('express')
+const dotenv = require('dotenv')
 const { DataSource } = require('typeorm');
 
+dotenv.config()
+
 const myDataSource = new DataSource({
- type: 'mysql', 
- host: 'localhost', 
- port: '3306',
- username: 'root',
- password: '',
- database: 'westagram'
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE
 })
+
+myDataSource.initialize()
+ .then(() => {
+    console.log("Data Source has been initialized!")
+ })
 
 
 const app = express()
@@ -48,8 +56,54 @@ app.get('/users', async(req, res) => {
 
 //2. users 생성
 
+app.post('/users', async(req, res) => {
+  try {
+
+    //1. user 정보를 frontend로부터 받는다.
+    const me = req.body
+
+    //2. user 정보 console.log로 확인
+    console.log("ME : ", me)
+
+    //3. DATABASE 정보 저장.
+    const name2 = me.name
+    const password2 = me.password
+    const email2 = me.email
+
+    const userData = await myDataSource.query(`
+      INSERT INTO users (
+        name,
+        password,
+        email
+      )
+      VALUES (
+        '${name2}',
+        '${password2}',
+        '${email2}'
+      )
+    `)
+
+    //4. DB data 저장 여부 확인
+    // : insertId는 주로 데이터 삽입 작업에서만 사용되며, 데이터베이스 작업의 성공 여부나 삽입된 (단일)레코드의 고유 식별자를 가져오는 데 사용
+    console.log('inserted user id : ', userData.insertId)
+
+    // 5. send response to FRONTEND
+		return res.status(201).json({
+      "message": "userCreated" 
+		})
+	} catch (err) {
+		console.log(err)
+	}
+})
 
 
+// 과제 4 UPDATE
+// 1번 user의 이름을 'Code Kim'으로 바꾸어 보세요.
+
+
+
+
+// 서버시작
 const server = http.createServer(app) // express app 으로 서버를 만듭니다.
 
 const start = async () => { // 서버를 시작하는 함수입니다.
@@ -60,9 +114,6 @@ const start = async () => { // 서버를 시작하는 함수입니다.
   }
 }
 
-myDataSource.initialize()
- .then(() => {
-    console.log("Data Source has been initialized!")
- })
+
 
 start()
